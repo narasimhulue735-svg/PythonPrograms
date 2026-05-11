d71e24f91b0e04ea0c9907c5f34599a360e77ebe
narasimhulu_naidu <narasimhulue735@gmail.com>
2026-05-11 16:45:35 +0530

    added new file

---
+++ operators.py
@@ -0,0 +1,112 @@
+''' Arthmetic operators:
++,-,*,/,//,%,** '''
+# a=10
+# b=30
+# print(a+b)
+# print(a-b)
+# print(a*b)
+# print(a/b)
+# print(a//b)
+# print(a%b)
+# print(a**b)
+'''Assignment operators:
+=,+=,-=,*=,/=,//=,%=,**= '''
+# a=10
+# a+=10
+# print(a)
+# a-=5
+# print(a)
+# a*=2
+# print(a)
+# a//=2
+# print(a)
+# a%=2
+# print(a)
+# a**=2
+# print(a)
+'''comparision operators:
+==,!=,>=,<=,<,>'''
+# a=10
+# b=20
+# print(a==b)
+# print(a!=b)
+# print(a>b)
+# print(a>=b)
+# print(a<=b)
+# print(a<b)
+'''identity operators:
+is ,is not'''
+# a=[10,20,30]
+# b=[10,20,30]
+# print(a is b)
+# print(a is not b)
+'''Bitwise operators:
+&,|,^,~,<<,>>'''
+# a=5 #bit-101
+# b=10 #bit-1010
+# print(~a)
+# print(a&b)
+# print(a|b)
+# print(a^b)
+# print(a>>b)
+# print(a<<b)
+'''logical operators:
+and,or,not:'''
+a=[1,2,3,4,5]
+b=[1,2,5,4]
+print(a and b)
+print(a or b)
+print(not a)
+
+'''Membership operators:
+in,not in'''
+# n="Narasimhulu Naidu"
+# print("Naidu" in n)
+# m=[1,2,3,4]
+# print(2 in m)
+# print(30 in m)
+'''Type casting:
+int(),str(),list(),tuple(),set(),dict()'''
+'''integer'''
+# a=10
+# a=str(a)
+# print(a)
+# print(type(a))
+'''string'''
+# n="Narasimhulu Naidu"
+# print(list(n))
+# print(tuple(n))
+# print(set(n))
+'''Tuple'''
+# t=(12,"Naidu",3,5,2)
+# t=str(t)
+# print(len(t))
+# print(type(t))
+# t=list(t)
+# print(t)
+# t=tuple(t)
+# print(t)
+'''list'''
+# l=[1,2,3,4]
+# print(l)
+# l=str(l)
+# print(l)
+# print(type(l))
+# l=tuple(l)
+# print(l)
+# print(type(l))
+# l=set(l)
+# print(l)
+# print(type(l))
+'''set'''
+# s={1,2,3,6,7}
+# print(s)
+# s=str(s)
+# print(s)
+# print(type(s))
+# s=tuple(s)
+# print(s)
+# print(type(s))
+# s=list(s)
+# print(s)
+# print(type(s))
